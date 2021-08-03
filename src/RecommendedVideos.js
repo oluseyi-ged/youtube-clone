dd3d0cc5f5b013347172fdadaeeb6a44041b2b97
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-03 12:25:07 +0100

    video card components styled

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,76 @@
 import React from "react"
 import "./RecommendedVideos.css"
+import VideoCard from "./VideoCard"
 
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
-      <h1>Recommended videos</h1>
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="
+HIGHLIGHTS | VFB Stuttgart 0-3 Barça (MEMPHIS, YUSUF..."
+          views="2.5M views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/UUUBRjUs2lBdKGgtOfsHifIXscTWf8HVf5xm0qUQsHPTDyGWqqKivQUmIKopoGYRIlU7W26lGA=s68-c-k-c0x00ffffff-no-rj"
+          channel="FC Barcelona"
+          image="https://i.ytimg.com/an_webp/41AFcPa2_F0/mqdefault_6s.webp?du=3000&sqp=CKyrpIgG&rs=AOn4CLADBbLC50qSsf3GA6rrXOL77e94xQ"
+        />
+        <VideoCard
+          title="
+HIGHLIGHTS | VFB Stuttgart 0-3 Barça (MEMPHIS, YUSUF..."
+          views="2.5M views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/UUUBRjUs2lBdKGgtOfsHifIXscTWf8HVf5xm0qUQsHPTDyGWqqKivQUmIKopoGYRIlU7W26lGA=s68-c-k-c0x00ffffff-no-rj"
+          channel="FC Barcelona"
+          image="https://i.ytimg.com/an_webp/41AFcPa2_F0/mqdefault_6s.webp?du=3000&sqp=CKyrpIgG&rs=AOn4CLADBbLC50qSsf3GA6rrXOL77e94xQ"
+        />
+        <VideoCard
+          title="
+HIGHLIGHTS | VFB Stuttgart 0-3 Barça (MEMPHIS, YUSUF..."
+          views="2.5M views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/UUUBRjUs2lBdKGgtOfsHifIXscTWf8HVf5xm0qUQsHPTDyGWqqKivQUmIKopoGYRIlU7W26lGA=s68-c-k-c0x00ffffff-no-rj"
+          channel="FC Barcelona"
+          image="https://i.ytimg.com/an_webp/41AFcPa2_F0/mqdefault_6s.webp?du=3000&sqp=CKyrpIgG&rs=AOn4CLADBbLC50qSsf3GA6rrXOL77e94xQ"
+        />
+        <VideoCard
+          title="
+HIGHLIGHTS | VFB Stuttgart 0-3 Barça (MEMPHIS, YUSUF..."
+          views="2.5M views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/UUUBRjUs2lBdKGgtOfsHifIXscTWf8HVf5xm0qUQsHPTDyGWqqKivQUmIKopoGYRIlU7W26lGA=s68-c-k-c0x00ffffff-no-rj"
+          channel="FC Barcelona"
+          image="https://i.ytimg.com/an_webp/41AFcPa2_F0/mqdefault_6s.webp?du=3000&sqp=CKyrpIgG&rs=AOn4CLADBbLC50qSsf3GA6rrXOL77e94xQ"
+        />
+        <VideoCard
+          title="
+HIGHLIGHTS | VFB Stuttgart 0-3 Barça (MEMPHIS, YUSUF..."
+          views="2.5M views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/UUUBRjUs2lBdKGgtOfsHifIXscTWf8HVf5xm0qUQsHPTDyGWqqKivQUmIKopoGYRIlU7W26lGA=s68-c-k-c0x00ffffff-no-rj"
+          channel="FC Barcelona"
+          image="https://i.ytimg.com/an_webp/41AFcPa2_F0/mqdefault_6s.webp?du=3000&sqp=CKyrpIgG&rs=AOn4CLADBbLC50qSsf3GA6rrXOL77e94xQ"
+        />
+        <VideoCard
+          title="
+HIGHLIGHTS | VFB Stuttgart 0-3 Barça (MEMPHIS, YUSUF..."
+          views="2.5M views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/UUUBRjUs2lBdKGgtOfsHifIXscTWf8HVf5xm0qUQsHPTDyGWqqKivQUmIKopoGYRIlU7W26lGA=s68-c-k-c0x00ffffff-no-rj"
+          channel="FC Barcelona"
+          image="https://i.ytimg.com/an_webp/41AFcPa2_F0/mqdefault_6s.webp?du=3000&sqp=CKyrpIgG&rs=AOn4CLADBbLC50qSsf3GA6rrXOL77e94xQ"
+        />
+        <VideoCard
+          title="
+HIGHLIGHTS | VFB Stuttgart 0-3 Barça (MEMPHIS, YUSUF..."
+          views="2.5M views"
+          timestamp="2 days ago"
+          channelImage="https://yt3.ggpht.com/UUUBRjUs2lBdKGgtOfsHifIXscTWf8HVf5xm0qUQsHPTDyGWqqKivQUmIKopoGYRIlU7W26lGA=s68-c-k-c0x00ffffff-no-rj"
+          channel="FC Barcelona"
+          image="https://i.ytimg.com/an_webp/41AFcPa2_F0/mqdefault_6s.webp?du=3000&sqp=CKyrpIgG&rs=AOn4CLADBbLC50qSsf3GA6rrXOL77e94xQ"
+        />
+      </div>
     </div>
   )
 }
